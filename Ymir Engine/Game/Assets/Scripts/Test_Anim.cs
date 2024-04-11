f6a26dfc45b300c8b6b21a0c55d2d13ec09d450d
BielRubio <bielrudi@gmail.com>
2024-04-11 20:25:19 +0200

    nuevo modelo de raisen

--- Ymir Engine/Game/Assets/Scripts/Test_Anim.cs
+++ Ymir Engine/Game/Assets/Scripts/Test_Anim.cs
@@ -10,27 +10,35 @@ public class Test_Anim : YmirComponent
 {
 	public void Start()
 	{
-		Animation.SetLoop(gameObject, "Idle", true);
-        Animation.SetLoop(gameObject, "Walk", true);
-        Animation.SetLoop(gameObject, "Run", true);
+		Animation.SetLoop(gameObject, "Raisen_Idle", true);
+        Animation.SetLoop(gameObject, "Raisen_Walk", true);
+        Animation.SetLoop(gameObject, "Raisen_Run", true);
+        Animation.SetLoop(gameObject, "Raisen_Dash", true);
 
-        Animation.AddBlendOption(gameObject, "Idle", "Walk", 20.0f);
-        Animation.AddBlendOption(gameObject, "Idle", "Run", 20.0f);
-        Animation.AddBlendOption(gameObject, "Idle", "Die", 20.0f);
+        Animation.SetResetToZero(gameObject, "Raisen_Die", false);
 
+        Animation.AddBlendOption(gameObject, "Raisen_Idle", "Raisen_Walk", 5.0f);
+        Animation.AddBlendOption(gameObject, "Raisen_Idle", "Raisen_Run", 5.0f);
+        Animation.AddBlendOption(gameObject, "Raisen_Idle", "Raisen_Die", 5.0f);
+        Animation.AddBlendOption(gameObject, "Raisen_Idle", "Raisen_Dash", 5.0f);
 
-        Animation.AddBlendOption(gameObject, "Walk", "Idle", 20.0f);
-        Animation.AddBlendOption(gameObject, "Walk", "Run", 20.0f);
-        Animation.AddBlendOption(gameObject, "Walk", "Die", 20.0f);
+        Animation.AddBlendOption(gameObject, "Raisen_Walk", "Raisen_Idle", 5.0f);
+        Animation.AddBlendOption(gameObject, "Raisen_Walk", "Raisen_Run", 5.0f);
+        Animation.AddBlendOption(gameObject, "Raisen_Walk", "Raisen_Die", 5.0f);
+        Animation.AddBlendOption(gameObject, "Raisen_Walk", "Raisen_Dash", 5.0f);
 
-        Animation.AddBlendOption(gameObject, "Run", "Idle", 20.0f);
-        Animation.AddBlendOption(gameObject, "Run", "Walk", 20.0f);
-        Animation.AddBlendOption(gameObject, "Run", "Die", 20.0f);
+        Animation.AddBlendOption(gameObject, "Raisen_Run", "Raisen_Idle", 5.0f);
+        Animation.AddBlendOption(gameObject, "Raisen_Run", "Raisen_Walk", 5.0f);
+        Animation.AddBlendOption(gameObject, "Raisen_Run", "Raisen_Die", 5.0f);
+        Animation.AddBlendOption(gameObject, "Raisen_Run", "Raisen_Dash", 5.0f);
 
+        Animation.AddBlendOption(gameObject, "Raisen_Dash", "Raisen_Idle", 5.0f);
+        Animation.AddBlendOption(gameObject, "Raisen_Dash", "Raisen_Walk", 5.0f);
+        Animation.AddBlendOption(gameObject, "Raisen_Dash", "Raisen_Die", 5.0f);
+        Animation.AddBlendOption(gameObject, "Raisen_Dash", "Raisen_Run", 5.0f);
 
-        Animation.PlayAnimation(gameObject, "Idle");
+        Animation.PlayAnimation(gameObject, "Raisen_Idle");
 
-        Animation.SetResetToZero(gameObject, "Die", false);
 
 
     }
@@ -38,15 +46,18 @@ public class Test_Anim : YmirComponent
 	public void Update()
 	{
         if (Input.GetKey(YmirKeyCode.W) == KeyState.KEY_DOWN)
-            Animation.PlayAnimation(gameObject, "Idle");
+            Animation.PlayAnimation(gameObject, "Raisen_Idle");
 
         if (Input.GetKey(YmirKeyCode.S) == KeyState.KEY_DOWN)
-            Animation.PlayAnimation(gameObject, "Walk");
+            Animation.PlayAnimation(gameObject, "Raisen_Walk");
 
         if (Input.GetKey(YmirKeyCode.A) == KeyState.KEY_DOWN)
-            Animation.PlayAnimation(gameObject, "Die");
+            Animation.PlayAnimation(gameObject, "Raisen_Die");
 
         if (Input.GetKey(YmirKeyCode.D) == KeyState.KEY_DOWN)
-            Animation.PlayAnimation(gameObject, "Run");
+            Animation.PlayAnimation(gameObject, "Raisen_Run");
+
+        if (Input.GetKey(YmirKeyCode.R) == KeyState.KEY_DOWN)
+            Animation.PlayAnimation(gameObject, "Raisen_Dash");
     }
 }
