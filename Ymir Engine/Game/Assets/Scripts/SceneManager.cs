2a4801e823846020fb5126e5c3bc49630191bcc0
Eric <ericluque2003@gmail.com>
2024-04-13 21:56:05 +0200

    Fix Collides Lvl. 2 Part 2 y SceneManager.cs

--- Ymir Engine/Game/Assets/Scripts/SceneManager.cs
+++ Ymir Engine/Game/Assets/Scripts/SceneManager.cs
@@ -12,25 +12,31 @@ public class SceneManager : YmirComponent
 	{
         if (Input.GetKey(YmirKeyCode.KP_1) == KeyState.KEY_DOWN)
         {
-            InternalCalls.LoadScene("Assets/Base_Blockout_Scene.yscene");
+            InternalCalls.LoadScene("Assets/BASE_FINAL/LVL_BASE_COLLIDERS.yscene");
             return;
         }
 
         if (Input.GetKey(YmirKeyCode.KP_2) == KeyState.KEY_DOWN)
         {
-            InternalCalls.LoadScene("Assets/MAPA_FINAL.yscene");
+            InternalCalls.LoadScene("Assets/LVL1_FINAL/LVL1_FINAL_COLLIDERS.yscene");
             return;
         }
 
         if (Input.GetKey(YmirKeyCode.KP_3) == KeyState.KEY_DOWN)
         {
-            InternalCalls.LoadScene("Assets/Lv1_Blockout_Scene.yscene");
+            InternalCalls.LoadScene("Assets/LVL2_LAB_PART1_FINAL/LVL2_LAB_PART1_COLLIDERS.yscene");
             return;
         }
 
         if (Input.GetKey(YmirKeyCode.KP_4) == KeyState.KEY_DOWN)
         {
-            InternalCalls.LoadScene("Assets/Lvl2_Blockout_Part2_Scene.yscene");
+            InternalCalls.LoadScene("Assets/LVL2_LAB_PART2_FINAL/LVL2_LAB_PART2_COLLIDERS.yscene");
+            return;
+        }
+
+        if (Input.GetKey(YmirKeyCode.KP_5) == KeyState.KEY_DOWN)
+        {
+            InternalCalls.LoadScene("Assets/LVL3_BlockOut/LVL3_BOSS_COLLDIERS.yscene");
             return;
         }
 
