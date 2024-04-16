4b04b72112a834252a72396c16c92ad6f35e9a68
Mario Garcia Sutil <mariosutil2003@gmail.com>
2024-04-16 21:19:44 +0200

    Añadida funcion para reproducir musica de los niveles

--- Ymir Engine/Source/ModuleScene.cpp
+++ Ymir Engine/Source/ModuleScene.cpp
@@ -89,7 +89,8 @@ bool ModuleScene::Start()
 	/*LoadSceneFromStart("Assets", "Enemigo player"); */
 	//LoadSceneFromStart("Assets/Test_Francesc", "TestPrefabs");
 	//LoadSceneFromStart("Assets", "Prueba enemigo lvl2");
-	LoadSceneFromStart("Assets/BASE_FINAL", "LVL_BASE_COLLIDERS");
+	//LoadSceneFromStart("Assets/BASE_FINAL", "LVL_BASE_COLLIDERS");
+	LoadSceneFromStart("Assets/LVL2_LAB_PART1_FINAL", "LVL2_LAB_PART1_COLLIDERS");
 	//LoadSceneFromStart("Assets", "Pollo Loco");
 
 #endif // _RELEASE
