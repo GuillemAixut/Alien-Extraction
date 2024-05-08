f64ff515a49fe77abc41b1d38138fead054ca6fc
Toni Romanos Ruiz <antonio.romanos@estudiantat.upc.edu>
2024-05-08 09:50:00 +0200

    WIP Player Particles
    Player particles with all guns (no tracer) and skills particles

--- Ymir Engine/Source/ModuleScene.cpp
+++ Ymir Engine/Source/ModuleScene.cpp
@@ -93,7 +93,7 @@ bool ModuleScene::Start()
 	// -------------------- FINAL MAPS -------------------- \\
 
 	//LoadScene("Assets/BASE_FINAL", "LVL_BASE_COLLIDERS");
-	LoadScene("Assets/LVL1_FINAL", "LVL1_FINAL_COLLIDERS");
+	//LoadScene("Assets/LVL1_FINAL", "LVL1_FINAL_COLLIDERS");
 	//LoadScene("Assets/LVL2_LAB_PART1_FINAL", "LVL2_LAB_PART1_COLLIDERS");
 	//LoadScene("Assets/LVL2_LAB_PART2_FINAL", "LVL2_LAB_PART2_COLLIDERS");
 	//LoadScene("Assets/LVL3_BlockOut", "LVL3_PART1_COLLIDERS");
