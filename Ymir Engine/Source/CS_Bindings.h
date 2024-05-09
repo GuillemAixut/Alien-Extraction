c3bad7989819179c213a55257fab4317ffb9d08d
xaita <xiao.shan.costajussa@estudiantat.upc.edu>
2024-05-09 14:56:19 +0200

    Fbx para los disparo

--- Ymir Engine/Source/CS_Bindings.h
+++ Ymir Engine/Source/CS_Bindings.h
@@ -982,6 +982,24 @@ void CreateSpitterAcidSpit(MonoObject* position, MonoObject* rotation)
 	go->mTransform->SetScale(scaleVector);
 	go->mTransform->SetRotation(rotVector);
 
+	uint UID = 1728623793; // UID of Cube.fbx mesh in meta (lo siento)
+
+	std::string libraryPath = External->fileSystem->libraryMeshesPath + std::to_string(UID) + ".ymesh";
+
+	//if (!PhysfsEncapsule::FileExists(libraryPath)) {
+
+	//	External->resourceManager->ImportFile("Assets/Primitives/Cube.fbx", true);
+
+	//}
+
+	//Añade la mesh a la bullet
+	ResourceMesh* rMesh = (ResourceMesh*)(External->resourceManager->CreateResourceFromLibrary(libraryPath, ResourceType::MESH, UID));
+	CMesh* cmesh = new CMesh(go);
+	cmesh->rMeshReference = rMesh;
+	go->AddComponent(cmesh);
+	CMesh* mesh;
+	mesh = new CMesh(go);
+
 	CCollider* physBody;
 	physBody = new CCollider(go, SPHERE);
 
