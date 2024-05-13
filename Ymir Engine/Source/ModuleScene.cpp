13136aec01c74055b41bf49abd22b38201732e5f
Eric <ericluque2003@gmail.com>
2024-05-13 20:46:08 +0200

    Loot Chest funcional cambiando el path a los items

--- Ymir Engine/Source/ModuleScene.cpp
+++ Ymir Engine/Source/ModuleScene.cpp
@@ -523,7 +523,10 @@ void ModuleScene::SavePrefab(GameObject* prefab, const std::string& dir, const s
 GameObject* ModuleScene::LoadPrefab(const std::string& dir, const std::string& fileName)
 {
 	TimeManager::gameTimer.Pause();
-	ClearVec(vTempComponents);
+	if (!vTempComponents.empty())
+	{
+		ClearVec(vTempComponents);
+	}
 	
 	std::unique_ptr<JsonFile> prefabToLoad = JsonFile::GetJSON(dir + "/" + fileName + ".yfab");
 
